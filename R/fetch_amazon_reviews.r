ab52155caed2d591a7cb6202e80629533b90c339
Sivaji Doguparthi <groups.sivaji@gmail.com>
2014-03-25 13:09:13 +0530

    Update fetch_amazon_reviews.r

--- R/fetch_amazon_reviews.r
+++ R/fetch_amazon_reviews.r
@@ -6,23 +6,26 @@
 #' @return data.frame containing the reviews with names
 #'   helpful_count, helpful_total, stars, title, date, description
 #' @export
-fetch_amazon_reviews <- function(id, page = 1) {
+fetch_amazon_reviews <- function (id, page = 1) 
+{
   library(RCurl)
   library(XML)
   curl <- getCurlHandle(useragent = "R", followlocation = TRUE)
-  URL <- paste0('http://www.amazon.com/my-favorite-book/product-reviews/', id)
-  if (page > 1) URL <- paste0(URL, '/ref=cm_cr_pr_top_link_', page, '?pageNumber=', page)
+  URL <- paste0("http://www.amazon.com/product-reviews/", id)
+  
+  if (page > 1) 
+    URL <- paste0(URL, "/cm_cr_pr_top_link?pageNumber=",page)
   raw.html <- getURL(URL, curl = curl)
   parsed.html <- htmlParse(raw.html)
-
-  if (page == 1) {
+  
+  if ((page == 1) && (length(xpathSApply(parsed.html, "//div[@class='CMpaginate']"))>1)) {
     pages <- as.vector(xpathSApply(parsed.html, "//div[@class='CMpaginate']"))[[1]]
-    page_count <- as.integer(gsub('.*([0-9]+)', '\\1', gsub(' \\| Next.*$', '', xmlValue(pages[[2]]))))
-    more_reviews <- lapply(seq_len(page_count - 1) + 1,
-                         function(page_num) fetch_amazon_reviews(id, page = page_num))
-  } else more_reviews <- NULL
-
-  reviews <- as.vector(xpathSApply(parsed.html, '//table[@id="productReviews"]/tr/td/div'))
+    page_count <- max(unique(na.omit(as.numeric(unlist(strsplit(gsub("[^/ 0-9]", '', xmlValue(pages[[2]])),split=" "))))))
+    more_reviews <- lapply(seq_len(page_count) , function(page_num) fetch_amazon_reviews(id,page = page_num))
+  }
+  else more_reviews <- NULL
+  
+  reviews <- as.vector(xpathSApply(parsed.html, "//table[@id=\"productReviews\"]/tr/td/div"))
   reviews_df <- do.call(rbind, lapply(reviews, review_to_df))
   do.call(rbind, append(more_reviews, list(reviews_df)))
 }
